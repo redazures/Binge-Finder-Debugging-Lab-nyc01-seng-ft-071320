0a4a8713bb95495b7d79d3cfbf7ab90f4f432b3d
redazures <jwu_nyc@yahoo.com>
2020-09-20 12:33:41 -0400

    fixed search

--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -7,9 +7,9 @@ class TVShowList extends Component {
   mapAllShows = () => {
     console.log("this is my search term in Tv Show", this.props.searchTerm)
     if (!!this.props.searchTerm){
-      this.props.shows.map((s) => {
+      return this.props.shows.map((s) => {
         if (s.name.toLowerCase().includes(this.props.searchTerm)){
-          (<TVShow show={s} key={s.id} selectShow={this.props.selectShow}/> )
+          return (<TVShow show={s} key={s.id} selectShow={this.props.selectShow}/> )
         }
       })
     }
